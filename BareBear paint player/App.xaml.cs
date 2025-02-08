71c9deccd64bdc993018293de680e2864202583a
petrlik <petrlik142@gmail.com>
2025-02-08 12:57:30 +0100

    Dependency injection

--- BareBear paint player/App.xaml.cs
+++ BareBear paint player/App.xaml.cs
@@ -1,14 +1,31 @@
-﻿using System.Configuration;
+﻿using BareBear_paint_player.Logic.Serialization;
+using Microsoft.Extensions.DependencyInjection;
+using System.Configuration;
 using System.Data;
 using System.Windows;
 
-namespace BareBear_paint_player
+namespace BareBear_paint_player;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
+    public App()
     {
+        // Dependency injection setup
+        ServiceCollection services = new();
+        ConfigureServices(services);
+
+        ServiceProvider servicesProvider = services.BuildServiceProvider();
+
+        var mainWindow = servicesProvider.GetRequiredService<MainWindow>();
+        mainWindow.Show();
     }
 
+    public void ConfigureServices(ServiceCollection services)
+    {
+        services.AddSingleton<IStreamManager, CanvasStreamManager>();
+        services.AddSingleton<MainWindow>();
+    }
 }
